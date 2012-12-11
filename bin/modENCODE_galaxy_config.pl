fdf51a89419e25db7c2fa9edd65a263409af7983
Ziru Zhou <zzhou@OLM-ZZHOU.local>
2012-12-11 11:17:43 -0500

    modified config script to use backup dependencies

--- bin/modENCODE_galaxy_config.pl
+++ bin/modENCODE_galaxy_config.pl
@@ -42,34 +42,58 @@ if (! -e $DEST_DIR) {
 }
 
 # ##################################
+# config bamedit
+system ("sudo cp -R $INPUT_DIR/bamedit $DEST_DIR");
+# extract bamedit dependencies and move it to appropriate directory
+# see http://wiki.g2.bx.psu.edu/Admin/Config/Tool%20Dependencies
+my $BAMEDIT_DIR="/mnt/galaxyTools/tools/samtools";
+# if BAMEDIT_DIR not exists, create it 
+if (! -e $BAMEDIT_DIR) {
+        system ("sudo mkdir $BAMEDIT_DIR");
+}
+# use backticks so outputs do not appear in console 
+my $cmd_out_bamedit = `sudo tar xvf modENCODE_DCC/bamedit/0.1.12.tar`;
+system ("sudo cp -R 0.1.12 $BAMEDIT_DIR/");
+system ("sudo rm -rf 0.1.12");
+if (! -e "$BAMEDIT_DIR/default") {
+        system ("sudo ln -s $BAMEDIT_DIR/0.1.12 $BAMEDIT_DIR/default");
+}
+
+# ##################################
 # config peakranger
 system ("sudo cp -R $INPUT_DIR/peakranger $DEST_DIR");
 # extract peakranger dependencies and move it to appropriate directory
 # see http://wiki.g2.bx.psu.edu/Admin/Config/Tool%20Dependencies
 my $PEAKRANGER_DIR="/mnt/galaxyTools/tools/peakranger";
-# if MAC2_DIR not exists, create it 
+# if PEAKRANGER_DIR not exists, create it 
 if (! -e $PEAKRANGER_DIR) {
         system ("sudo mkdir $PEAKRANGER_DIR");
 }
 # use backticks so outputs do not appear in console 
 my $cmd_out_ranger = `sudo tar xvf modENCODE_DCC/peakranger/1.16.tar`;
 system ("sudo cp -R 1.16 $PEAKRANGER_DIR/");
-system ("sudo rm -rf 1.16 ");
+system ("sudo rm -rf 1.16");
 if (! -e "$PEAKRANGER_DIR/default") {
         system ("sudo ln -s $PEAKRANGER_DIR/1.16 $PEAKRANGER_DIR/default");
 }
 
 # ##################################
-# config SPP
-system ("sudo cp -R $INPUT_DIR/spp $DEST_DIR");
-
-# ##################################
-# config IDR 
-system ("sudo cp -R $INPUT_DIR/idr $DEST_DIR");
-
-# ##################################
 # config macs
 system ("sudo cp -R $INPUT_DIR/macs $DEST_DIR");
+# extract macs dependencies and move it to appropriate directory
+# see http://wiki.g2.bx.psu.edu/Admin/Config/Tool%20Dependencies
+my $MACS_DIR="/mnt/galaxyTools/tools/macs";
+# if MACS_DIR not exists, create it 
+if (! -e $MACS_DIR) {
+	system ("sudo mkdir $MACS_DIR");
+}
+# use backticks so outputs do not appear in console 
+my $cmd_out_macs = `sudo tar xvf modENCODE_DCC/macs/1.4.1.tar`; 
+system ("sudo cp -R 1.4.1 $MACS_DIR/");
+system ("sudo rm -rf 1.4.1");
+if (! -e "$MACS_DIR/default") {
+	system ("sudo ln -s $MACS_DIR/1.4.1 $MACS_DIR/default");
+}
 
 # ##################################
 # config macs2
@@ -84,23 +108,37 @@ if (! -e $MACS2_DIR) {
 # use backticks so outputs do not appear in console 
 my $cmd_out_macs2 = `sudo tar xvf modENCODE_DCC/macs2/2.0.10.2.tar`; 
 system ("sudo cp -R 2.0.10.2 $MACS2_DIR/");
-system ("sudo rm -rf 2.0.10.2 ");
+system ("sudo rm -rf 2.0.10.2");
 if (! -e "$MACS2_DIR/default") {
 	system ("sudo ln -s $MACS2_DIR/2.0.10.2 $MACS2_DIR/default");
 }
+
+# ##################################
+# config SPP
+system ("sudo cp -R $INPUT_DIR/spp $DEST_DIR");
+
+# ##################################
+# config IDR 
+system ("sudo cp -R $INPUT_DIR/idr $DEST_DIR");
 
 # ##################################
 # config faceted browser
 system ("sudo cp -R $INPUT_DIR/faceted_browser $DEST_DIR");
 
 # ##################################
-# config bamedit 
-system ("sudo cp -R $INPUT_DIR/bamedit $DEST_DIR");
-
-# ##################################
 # change ownership and give rx permissions 
 system ("sudo chown -R galaxy:galaxy $MACS2_DIR");
 system ("sudo chmod -R a+rx $MACS2_DIR");
+
+system ("sudo chown -R galaxy:galaxy $MACS_DIR");
+system ("sudo chmod -R a+rx $MACS_DIR");
+
+system ("sudo chown -R galaxy:galaxy $PEAKRANGER_DIR");
+system ("sudo chmod -R a+rx $PEAKRANGER_DIR");
+
+system ("sudo chown -R galaxy:galaxy $BAMEDIT_DIR");
+system ("sudo chmod -R a+rx $BAMEDIT_DIR");
+
 system ("sudo chown -R galaxy:galaxy $DEST_DIR");
 system ("sudo chmod -R a+rx $DEST_DIR");
 
