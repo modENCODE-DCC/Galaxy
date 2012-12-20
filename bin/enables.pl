c6576f4921ad978f113765ecbc6c069eedf346ed
Quang Trinh <quang.trinh@gmail.com>
2012-12-20 09:05:33 -0500

    added bin/enables.pl for enabling both macs2 and spp

---
+++ bin/enables.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl 
+
+#
+# written by modENCODE DCC group
+# please send your questions/comments to help@modencode.org 
+#
+
+use strict;
+use warnings;
+
+system ("bin/macs2_enable.pl");
+system ("bin/spp_enable.pl");
+
