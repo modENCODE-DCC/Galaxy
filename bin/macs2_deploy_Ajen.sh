76f9e1041d8cd7b567b2396ab912267eb514f472
Fjen <arthur20249@gmail.com>
2013-01-09 13:21:21 -0500

    Created a dir for macs2 to log error msg to, and wrapped installation process in two functions

--- bin/macs2_deploy_Ajen.sh
+++ bin/macs2_deploy_Ajen.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 python_installed=`python -V 2>&1`
-numpy_installed=`pip freeze | grep numpy`
+numpy_installed=`pip freeze 2>/dev/null | grep numpy`
+hostname=`hostname`
 
-if [ "$python_installed" == "Python 2.7.3" ]; 
-then
-    date=`date`
-    echo "$date : Python 2.7.3 has been installed. Don't need to install python!" >> /var/log/macs2_deploy
-else
+function create_dir ( )
+{
+	if [[ -d "/var/log/macs2_installation" ]]; then
+		echo -e "$hostname:\n  - Log file directory exists ... skip creating dir ... continuing the installation"
+		echo ""
+	else
+		sudo mkdir -p /var/log/macs2_installation
+		echo -e "$hostname:	\n  -Log file directory has been created.\n  -You can access log files: /var/log/macs2_installation"
+		echo ""
+	fi	
+}
+
+
+function python_install ( )
+{
 	cd 
     #get python
     #mkdir python2.7.3
@@ -23,24 +34,60 @@ else
 	sudo chmod ag+w /usr/local/lib/python2.7/site-packages
 	sudo chmod ag+wx /usr/local/bin
 	date=`date`
-    echo "$date : Python 2.7.3 is installed" >> /var/log/macs2_deploy
+    echo "$date : Python 2.7.3 is installed" >> /var/log/macs2_installation/python_log
     #remove downloaded files
 	cd
 	rm -rf Python-2.7.3
-fi
+}
 
-if [ "$numpy_installed" == "numpy==1.3.0" ]; 
-then
-	date=`date`
-	echo "$date : python package - numpy 1.3.0 has been installed. Don't need to install numpy!" >> /var/log/macs2_deploy
-else
+function numpy_install ( )
+{
 	#install numpy
 	git clone git://github.com/numpy/numpy.git numpy
 	cd numpy
 	python setup.py build
 	python setup.py install
 	date=`date`
-	echo "$date : python package - numpy 1.3.0 is installed" >> /var/log/macs2_deploy	
+	echo "$date : python package - numpy 1.3.0 is installed" >> /var/log/macs2_installation/numpy_log
+}
+
+
+
+#Function calls
+#====================================
+
+create_dir
+
+#Python: log installation outputs to logfile and also send them back to main concole to update users
+if [ "$python_installed" == "Python 2.7.3" ]; 
+then
+    date=`date`
+    echo -e "$hostname:\n   -Python 2.7.3 has been installed. Don't need to install python!"
+    echo ""
+    echo "$date : Python 2.7.3 has been installed. Don't need to install python!" >> /var/log/macs2_installation/python_log
+else
+	echo "Installing $python_installed on $hostname. It may take a few minutes ..."
+	echo ""
+	#Call python_install function to install dependencies
+	python_install >> /var/log/macs2_installation/python_install.log 2>> /var/log/macs2_installation/python_error.log 
+	echo "Installation of macs2 dependencies on $hostname completed ..."
+	echo ""
+fi
+
+#Numpy: log installation outputs to logfile and also send them back to main concole to update users
+if [ "$numpy_installed" == "numpy==1.3.0" ]; 
+then
+	date=`date`
+	echo -e "$hostname:\n   -python package - numpy 1.3.0 has been installed. Don't need to install numpy!"
+	echo ""
+	echo "$date : python package - numpy 1.3.0 has been installed. Don't need to install numpy!" >> /var/log/macs2_installation/numpy_log
+else
+	echo "Installing $numpy_installed on $hostname. It may take a few minutes ..."
+	echo ""
+	#Call numpy_install function to install dependencies 
+	numpy_install >> /var/log/macs2_installation/numpy_install.log 2>> /var/log/macs2_installation/numpy_error
+	echo "Installation of macs2 dependencies on $hostname completed ..."
+	echo ""
 fi
 
 
