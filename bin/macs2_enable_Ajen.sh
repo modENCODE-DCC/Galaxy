104e42e34edd48bcc18e47434407df5069644f6e
Fjen <arthur20249@gmail.com>
2013-01-09 13:16:52 -0500

    Remove all the echo statment, and move them to macs2_deploy file

--- bin/macs2_enable_Ajen.sh
+++ bin/macs2_enable_Ajen.sh
@@ -23,34 +23,41 @@ function CopyInstaller ( )
 	sudo cp bin/macs2_deploy.sh /mnt/galaxyData/tmp
 }
 
+
 #Execute the script on computing node
 #===========================================
 function Execute ( )
 {
+	#process the nodes 
 	if [[ "$1" == dom* ]]; 
 	then
 		1="$1.compute-1.internal"
-		sudo ssh $1 -o "StrictHostKeyChecking no" "bash -s" < /mnt/galaxyData/tmp/123.sh
+		#echo "Installing dependencies on $1. It may take a few minutes ..."
+		sudo ssh $1 -o "StrictHostKeyChecking no" "bash -s" < /mnt/galaxyData/tmp/macs2_deploy_Ajen.sh
 	elif [[ "$1" == "$hostname" ]]; then
-		bash /mnt/galaxyData/tmp/123.sh
+		#echo "Installing dependencies on $1. It may take a few minutes ..."
+		bash /mnt/galaxyData/tmp/macs2_deploy_Ajen.sh
 	else
-		sudo ssh $1 -o "StrictHostKeyChecking no" "bash -s" < /mnt/galaxyData/tmp/123.sh
+		#echo "Installing dependencies on $1. It may take a few minutes ..."
+		sudo ssh $1 -o "StrictHostKeyChecking no" "bash -s" < /mnt/galaxyData/tmp/macs2_deploy_Ajen.sh
 	fi 
 }
 
 
 
 #Function Calls
-#===================================
-
+#============================================
 ComputingNodes
 CopyInstaller
 
-#Calling Execution function
+#Calling Execution function. 
+#All the dependencies will be installed on computing nodes at the same time.
 for nodes in $NodesList; do
 	Execute ${nodes} &
 done
 
+#Wait till the process to finish in parallel 
 wait
 
-echo "Done ..."
+echo "Done: All dependencies have been installed on computing nodes. You can start analizing your data ..."
+
