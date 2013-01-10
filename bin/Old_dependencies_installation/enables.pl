d7c0221aaa4100ffb6373d05cce9e6ac99fd4238
Fjen <arthur20249@gmail.com>
2013-01-10 10:18:17 -0500

    tmp dir to store old scripts

---
+++ bin/Old_dependencies_installation/enables.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl 
+
+#
+# written by modENCODE DCC group
+# please send your questions/comments to help@modencode.org 
+#
+
+use strict;
+use warnings;
+
+#system ("bin/macs2_enable.pl");
+#system ("bin/spp_enable.pl");
+system ("bin/")
+system ("bin/modENCODE_galaxy_restart.pl");
+
