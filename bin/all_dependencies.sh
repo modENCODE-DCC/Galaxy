e7808577463a046c7318e701e48ba5935847a5a6
Fjen <arthur20249@gmail.com>
2013-01-14 16:53:11 -0500

    Added "Node_Type" variable to indicate the type of the host: Master/Computing node

--- bin/all_dependencies.sh
+++ bin/all_dependencies.sh
@@ -11,9 +11,9 @@ hostname=`hostname`
 
 #Install spp
 rm -rf /usr/local/lib/R/site-library/00LOCK
-echo -e "$hostname:\n   -Installing spp. It may take a few minutes ..."
+echo -e "$hostname $Node_Type:\n   -Installing spp. It may take a few minutes ..."
 echo ""
 #use /dev/null to suppress the creation of the .Rout file.
 R CMD BATCH /mnt/galaxyData/tmp/spp_deploy.R /dev/null
-echo -e "$hostname:\n   -Installation of spp completed ... "
+echo -e "$hostname $Node_Type:\n   -Installation of spp completed ... "
 echo ""
