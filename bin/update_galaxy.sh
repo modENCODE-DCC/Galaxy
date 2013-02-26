70ee7729b910cfcd7e99c9502e53f112b171e88a
Quang Trinh <quang.trinh@gmail.com>
2013-02-26 13:06:13 -0500

    added bin/update_galaxy.sh

---
+++ bin/update_galaxy.sh
@@ -0,0 +1,20 @@
+
+
+# default Galaxy version on Amazon 8140:8afb981f46c1
+
+sudo su galaxy 
+
+cd /mnt/galaxyTools/galaxy-central
+
+# hg pull 
+hg checkout stable 
+
+# hg update release_2013.02.08 
+
+# Then to get all updates to the stable branch, you can:
+
+# hg pull -b stable https://bitbucket.org/galaxy/galaxy-central
+
+hg update stable 
+
+
