8ee2ffc2c5e72d20b9784c97f91d45cf1398e140
Quang Trinh <quang.trinh@gmail.com>
2013-03-03 06:52:32 -0500

    updated bin/update_galaxy.pl to include update database

--- bin/update_galaxy.pl
+++ bin/update_galaxy.pl
@@ -6,19 +6,14 @@ use strict;
 
 # default Galaxy version on Amazon 8140:8afb981f46c1
 
-
+print "\n\nupdate Galaxy to get the latest copy ...";
 system("cd /mnt/galaxyTools/galaxy-central; sudo -u galaxy hg pull; sudo -u galaxy hg checkout stable; sudo -u galaxy hg update stable");
-
-#system("sudo -u galaxy hg pull"); 
-
-#system("sudo -u galaxy hg checkout stable"); 
 
 # hg update release_2013.02.08 
 
-# Then to get all updates to the stable branch, you can:
+print "\n\nupdate Galaxy database to latest version ...");
+system("cd /mnt/galaxyTools/galaxy-central; sudo -u galaxy sh manage_db.sh upgrade");
 
-# hg pull -b stable https://bitbucket.org/galaxy/galaxy-central
-
-#system("sudo -u galaxy hg update stable");	
+print "\n\n";
 
 
