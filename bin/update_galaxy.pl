5c19f8fb59ba74e416c1658bd4bc524b66cc9fd1
Quang Trinh <quang.trinh@gmail.com>
2013-03-03 07:07:53 -0500

    updated bin/update_galaxy.pl

--- bin/update_galaxy.pl
+++ bin/update_galaxy.pl
@@ -6,12 +6,14 @@ use strict;
 
 # default Galaxy version on Amazon 8140:8afb981f46c1
 
-print "\n\nupdate Galaxy to get the latest copy ...";
+print "\n\n===========================================";
+print "\nupdate Galaxy to get the latest copy ...";
 system("cd /mnt/galaxyTools/galaxy-central; sudo -u galaxy hg pull; sudo -u galaxy hg checkout stable; sudo -u galaxy hg update stable");
 
 # hg update release_2013.02.08 
 
-print "\n\nupdate Galaxy database to latest version ...";
+print "\n\n===========================================";
+print "\nupdate Galaxy database to latest version ...";
 system("cd /mnt/galaxyTools/galaxy-central; sudo -u galaxy sh manage_db.sh upgrade");
 
 print "\n\n";
